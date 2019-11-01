a98c5d3da86980ea0a2ec555ea37b6803fc7c63f
Scary MrGrey <imbamrgrey@gmail.com>
2019-11-01 15:06:17 +0100

    changed load

--- src/test/scala/TxInsertionSpec.scala
+++ src/test/scala/TxInsertionSpec.scala
@@ -8,7 +8,7 @@ class TxInsertionSpec extends FlatSpec with Matchers {
 
   "Transaction" should "update and insert entities concurrently" in {
     implicit val s: Storage = TestAux.initStorage
-    val numJobs = 10000
+    val numJobs = 50000
     val numThreads = 8
 
     implicit val ec1: ExecutionContextExecutor = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(numThreads))
